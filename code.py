7f6dd4d136128663a351092f768cbce989ca87a2
Jeffry Valverde <jvalverdef@ucenfotec.ac.cr>
2024-06-13 22:22:56 -0600

    Agrega clase 'web_server_controller'

--- code.py
+++ code.py
@@ -1,154 +1,71 @@
-import mdns
-import socketpool
-import wifi
-from adafruit_httpserver import Server, Request, JSONResponse, POST, FileResponse
-from ideaboard import IdeaBoard
-from interpreter import Interpreter
-from time import sleep
+from ideaboard import IdeaBoard  
+from interpreter import Interpreter  # Importa la clase Interpreter para interpretar y ejecutar comandos.
+from web_server_controller import WebServerController  # Importa la clase WebServerController para controlar el servidor web.
+from time import sleep 
 import secretos
 
+# Crear instancias de IdeaBoard, Interpreter y WebServerController.
 ib = IdeaBoard()
 inter = Interpreter()
+wsc = WebServerController()
 
-# define functions (using non just to get a None parameter)
+# Función para detener los motores y apagar el LED.
 def stop(non):
-    # detiene los motores
-    ib.pixel = (0,0,0)
-    ib.motor_1.throttle = 0
+    ib.pixel = (0,0,0)  
+    ib.motor_1.throttle = 0  
     ib.motor_2.throttle = 0
-    
+
+# Función para avanzar hacia adelante.
 def forward(non):
-    # Mueve el robot hacia adelante
-    # por tiempo 3, a velocidad speed = [0,5]    
     ib.pixel = (0,255,0)
-    ib.motor_1.throttle = 0.5
-    ib.motor_2.throttle = 0.5
-    sleep(3)
-    stop(None)
-    
+    ib.motor_1.throttle = 0.5  
+    ib.motor_2.throttle = 0.5  
+    sleep(3)  
+    stop(None) 
+    sleep(0.5) 
+
+# Función para retroceder.
 def backward(non):
-    # Mueve el robot hacia atrás
-    # por tiempo 3, a velocidad speed = [0,5]    
-    ib.pixel = (150,255,0)
-    ib.motor_1.throttle = -0.5
-    ib.motor_2.throttle = -0.5
-    sleep(3)
-    stop(None)
-    
+    ib.pixel = (150,255,0) 
+    ib.motor_1.throttle = -0.5 
+    ib.motor_2.throttle = -0.5 
+    sleep(3) 
+    stop(None)  
+    sleep(0.5)
+
+# Función para girar a la izquierda.
 def left(non):
-    # Mueve el robot hacia la izquierda
-    # por tiempo 3, a velocidad speed = [0,5]    
-    ib.pixel = (50,55,100)
+    ib.pixel = (50,55,100) 
     ib.motor_1.throttle = -0.5
-    ib.motor_2.throttle = 0.5
-    sleep(3)
+    ib.motor_2.throttle = 0.5 
+    sleep(3) 
     stop(None)
-    
+    sleep(0.5)
+
+# Función para girar a la derecha.
 def right(non):
-    # Mueve el robot hacia la derecha
-    # por tiempo 3, a velocidad speed = [0,5]    
     ib.pixel = (50,55,100)
     ib.motor_1.throttle = 0.5
     ib.motor_2.throttle = -0.5
     sleep(3)
     stop(None)
-    
-# add functions to interpreter ('name', (function, parameter))
-inter.addFunction('stop',(stop,None))
-inter.addFunction('forward',(forward,None))
-inter.addFunction('backward',(backward,None))
-inter.addFunction('left',(left,None))
-inter.addFunction('right',(right,None))
+    sleep(0.5)
 
-print(f"Connecting to {secretos.CIRCUITPY_WIFI_SSID}...")
-wifi.radio.connect(secretos.CIRCUITPY_WIFI_SSID, secretos.CIRCUITPY_WIFI_PASSWORD)
-print(f"Connected to {secretos.CIRCUITPY_WIFI_SSID}...")
+# Agrega las funciones al intérprete para que puedan ser llamadas a través de comandos.
+inter.addFunction('stop', (stop, None))
+inter.addFunction('forward', (forward, None))
+inter.addFunction('backward', (backward, None))
+inter.addFunction('left', (left, None))
+inter.addFunction('right', (right, None))
 
-pool = socketpool.SocketPool(wifi.radio)
-server = Server(pool, debug=False)
-puerto = 80
+# Configuración del servidor web para servir archivos estáticos y manejar solicitudes.
+wsc.add_standard_decorator("/", "index.html", "/")
+wsc.add_standard_decorator("/css/reset.css", "reset.css", "/css")
+wsc.add_standard_decorator("/css/styles.css", "styles.css", "/css")
+wsc.add_standard_decorator("/js/dragAndDrop.js", "dragAndDrop.js", "/js")
+wsc.add_standard_decorator("/assets/cenfotec-logo.png", "cenfotec-logo.png", "/assets")
+wsc.add_standard_decorator("/assets/crcibernetica-logo.png", "crcibernetica-logo.png", "/assets")
+wsc.add_json_response_decorator("/data")
 
-mdns_server = mdns.Server(wifi.radio)
-mdns_server.hostname = secretos.HOSTNAME
-mdns_server.advertise_service(service_type="_http", protocol="_tcp", port=80)
-
-@server.route("/")
-def base(request: Request):
-    """
-    Serve the default index.html file.
-    """
-
-    return FileResponse(request, "index.html", "/")
-
-@server.route("/css/reset.css")
-def base(request: Request):
-    """
-    Serve the file.
-    """
-
-    return FileResponse(request, "reset.css", "/css")
-
-@server.route("/css/styles.css")
-def base(request: Request):
-    """
-    Serve the file.
-    """
-
-    return FileResponse(request, "styles.css", "/css")
-
-@server.route("/js/dragAndDrop.js")
-def base(request: Request):
-    """
-    Serve the file.
-    """
-
-    return FileResponse(request, "dragAndDrop.js", "/js")
-
-@server.route("/assets/cenfotec-logo.png")
-def base(request: Request):
-    """
-    Serve the file.
-    """
-    return FileResponse(request, "cenfotec-logo.png", "/assets")
-
-@server.route("/assets/crcibernetica-logo.png")
-def base(request: Request):
-    """
-    Serve the file.
-    """
-    return FileResponse(request, "crcibernetica-logo.png", "/assets")
-
-@server.route("/data", [POST], append_slash=True)
-def api(request: Request):
-    if request.method == POST:
-        # Parse JSON data from the request body
-        try:
-            uploaded_object = request.json()
-        except ValueError:
-            return JSONResponse(request, {"message": "Invalid JSON format"}, status_code=400)
-
-        # Print the received object to the console
-        print("Recibido:", uploaded_object["dato"])
-        recibido = uploaded_object["dato"]
-        inter.execute(recibido)
-
-        # Add the necessary CORS headers
-        headers = {"Access-Control-Allow-Origin": "*"}  # Permitir todas las solicitudes CORS
-
-        # Return a JSON response indicating success
-        return JSONResponse(request, {"message": "Object received", "object": uploaded_object}, headers=headers)
-    return JSONResponse(request, {"message": "Method not allowed"}, status_code=405)
-
-# Manejador de ruta para solicitudes OPTIONS
-@server.route("/data", ["OPTIONS"], append_slash=True)
-def options(request: Request):
-    # Agregar los encabezados CORS necesarios para las solicitudes OPTIONS
-    headers = {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "POST",
-        "Access-Control-Allow-Headers": "Content-Type",
-    }
-    return JSONResponse(request, {}, headers=headers)
-
-print("http://" + mdns_server.hostname + ".local" + "/")
-server.serve_forever(str(wifi.radio.ipv4_address), port=puerto)
+ib.pixel = (0,0,255)  # Enciende el LED en azul al para indicar que esta listo para recibir mensajes.
+wsc.execute_server(inter)  # Inicia el servidor web con el intérprete.
---
+++ web_server_controller.py
@@ -0,0 +1,71 @@
+# Author: Jeffry Valverde Fallas
+# Controlador de servidor web, pensado para interactuar con interpreter.py
+import mdns
+import socketpool
+import wifi
+import secretos
+from adafruit_httpserver import Server, Request, JSONResponse, POST, FileResponse
+
+# Clase para manejar el servidor local
+class WebServerController:
+    def __init__(self):
+        self.decorators = []
+
+    # Método para añadir un decorador estándar que sirve archivos
+    def add_standard_decorator(self, route_name, file_name, folder_name):
+        self.decorators.append(["add_standard_decorator", route_name, file_name, folder_name])
+        
+    # Método para añadir un decorador que maneja respuestas JSON en rutas POST
+    def add_json_response_decorator(self, route_name):
+        self.decorators.append(["add_json_response_decorator", route_name])
+        
+    # Método para configurar y ejecutar el servidor
+    def execute_server(self, interpreter):        
+        # Conecta a la red WiFi
+        print(f"Connecting to {secretos.CIRCUITPY_WIFI_SSID}...")
+        wifi.radio.connect(secretos.CIRCUITPY_WIFI_SSID, secretos.CIRCUITPY_WIFI_PASSWORD)
+        print(f"Connected to {secretos.CIRCUITPY_WIFI_SSID}...")
+
+        # Crea un pool de sockets y un servidor HTTP
+        pool = socketpool.SocketPool(wifi.radio)
+        server = Server(pool, debug=False)
+        puerto = 80
+
+        # Configura el servidor mDNS
+        mdns_server = mdns.Server(wifi.radio)
+        mdns_server.hostname = secretos.HOSTNAME
+        mdns_server.advertise_service(service_type="_http", protocol="_tcp", port=puerto)
+
+        # Itera sobre los decoradores y añade las rutas al servidor
+        for decorator in self.decorators:
+            if decorator[0] == "add_standard_decorator":
+                route_name, file_name, folder_name = decorator[1], decorator[2], decorator[3]
+                
+                @server.route(route_name)
+                def base(request: Request, file_name=file_name, folder_name=folder_name):
+                    return FileResponse(request, file_name, folder_name)
+                    
+            elif decorator[0] == "add_json_response_decorator":
+                route_name = decorator[1]
+                
+                @server.route(route_name, [POST], append_slash=True)
+                def api(request: Request):
+                    if request.method == POST:
+                        try:
+                            uploaded_object = request.json()
+                        except ValueError:
+                            return JSONResponse(request, {"message": "Invalid JSON format"}, status_code=400)
+
+                        recibido = uploaded_object["dato"]
+                        print(recibido)  # Debug
+                        interpreter.execute(recibido)
+
+                        headers = {"Access-Control-Allow-Origin": "*"}
+                        return JSONResponse(request, {"message": "Object received", "object": uploaded_object}, headers=headers)
+                    return JSONResponse(request, {"message": "Method not allowed"}, status_code=405)
+            else:
+                print('Function not found')
+        
+        # Imprime la URL del servidor y lo inicia
+        print("http://" + mdns_server.hostname + ".local/")
+        server.serve_forever(str(wifi.radio.ipv4_address), port=puerto)
