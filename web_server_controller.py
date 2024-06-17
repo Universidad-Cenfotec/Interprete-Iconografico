cef281b0f72b6aaacee169966c7a1cf9fd1d79ac
Jeffry Valverde <jeffry18vf@gmail.com>
2024-06-17 10:03:22 -0600

    Licencias

--- web_server_controller.py
+++ web_server_controller.py
@@ -1,8 +1,8 @@
 '''
 ***************************************************
-  Universidad CENFOTEC invierte tiempo y recursos en el desarrollo de 
-  contenidos Open Source. Apoye las actividades de la Universidad,
-  y cualqueir modificación compártala de forma abierta
+  Universidad CENFOTEC invierte tiempo y recursos en el desarrollo de
+  contenidos Open Source. Apoye las actividades de la universidad
+  y comparta cualquier modificación de forma abierta.
 
   Desarrollado por Jeffry Valverde y Bentley Born
   MIT license, all text above must be included in any redistribution
